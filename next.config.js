d627487e0f42a7acb8ca9d62e23105c79d116f35
albugowy15 <muhammadbughowy@gmail.com>
2022-09-25 09:59:23 +0700

    install required dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-import { withContentlayer } from "next-contentlayer/.";
-const nextConfig = {
+// import { withContentlayer } from "next-contentlayer";
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
+
+// module.exports = withContentlayer(nextConfig);
+
+const { withContentlayer } = require("next-contentlayer");
+
+module.exports = withContentlayer({
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = withContentlayer(nextConfig);
+  images: {
+    domains: [
+      "user-images.githubusercontent.com",
+      "images.unsplash.com",
+      "localhost:3000",
+    ],
+  },
+});
